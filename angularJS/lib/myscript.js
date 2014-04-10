b106e8fb399630c67dd6b419f541291051410abf
Keisuke Sakamoto <corderia_gray_ride_rgm_79n@yahoo.co.jp>
2014-04-10 21:19:49 +0900

    anglarJS lesson 6 ended.

---
+++ angularJS/lib/myscript.js
@@ -0,0 +1,14 @@
+var mainCtrl = function($scope){
+	$scope.users = [
+		{"name":"sakmto","score":10.5},
+		{"name":"inb","score":110.5},
+		{"name":"inue","score":105.34},
+		{"name":"sakmto","score":10.5},
+		{"name":"inb","score":110.5},
+		{"name":"inue","score":105.34},
+		{"name":"sakmto","score":10.5},
+		{"name":"inb","score":110.5},
+		{"name":"inue","score":105.34},
+	];
+	$scope.today = new Date();
+}
