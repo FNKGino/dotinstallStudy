fe88912bcb4f215204532c10be94cac300f4f7cb
Keisuke Sakamoto <corderia_gray_ride_rgm_79n@yahoo.co.jp>
2014-04-12 11:14:43 +0900

    angularJs ended.

--- angularJS/lib/myscript.js
+++ angularJS/lib/myscript.js
@@ -1,4 +1,5 @@
 var mainCtrl = function($scope){
+	//User配列
 	$scope.users = [
 		{"name":"sakmto","score":10.5},
 		{"name":"inb","score":110.5},
@@ -10,5 +11,16 @@ var mainCtrl = function($scope){
 		{"name":"inb","score":110.5},
 		{"name":"inue","score":105.34},
 	];
+	//今日の日付を取得
 	$scope.today = new Date();
+	// Userを追加する
+	$scope.addUser = function(){
+
+	}
+}
+var userItemCtrl = function($scope){
+	//+1ボタンを押された時の挙動
+	$scope.increment = function(){
+		$scope.user.score++;
+	}
 }
